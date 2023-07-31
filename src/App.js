515b127487a559a28dc4c97bbf002b88445d9aed
pankajajoshi <joshi.pankaja@gmail.com>
2023-07-31 14:13:26 -0400

    added mealinfo
    new bg img
    
    created recipe page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import logo from './logo.svg';
-import React, { useState } from "react";
-import './style.css';
-import Meal from './Components/Meal';
+import React, { useEffect, useState } from "react";
+ import './style.css';
+ import{ MealInfo } from './Components/MealInfo';
+import { Categories } from './Components/Categories';
+//  <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,600;1,400&family=Fuzzy+Bubbles&family=Roboto+Slab:wght@900&family=Satisfy&display=swap" rel="stylesheet"></link>
 
 
 
 function App() {
- 
-			
+
+  const [search,setSearch] =useState('')
+  const [mealInfoData,setMealInfoData]=useState()
+  const[categoriesData,setCategoiesData]=useState()
+  const getData=async() =>{
+    const res=await fetch('https://www.themealdb.com/api/json/v1/1/categories.php?q=${search}')
+    const resData = await res.json();
+    setCategoiesData(resData)
+  //   console.log(resData)
+  }
+			useEffect(()=>{
+        getData()
+
+      },[search])
       return (
 		<>
         
-      <Meal />
-        
+        <div className="header">
+          <h1>Recipes-for-Everyone</h1>
+          <div className="search-box"> SEARCH
+            <input type="search" placeholder='search meal....' onChange={(e)=>setSearch(e.target.valueAsNumber)}/>
+
+          </div>
+        </div>
+        <div className="container">
+          <div className="mealInfo">
+            {mealInfoData && <MealInfo mealInfoData ={mealInfoData}/>}
+          </div>
+          <div className="meal-row">
+            <h2 className="text-heading">Categories</h2>
+            <div className="row">
+              
+              <Categories categorylist={categoriesData}
+              setMealInfoData={setMealInfoData} />
+            </div>
+
+          </div>
+        </div>
 		</>
 	);
 }
---
+++ src/Components/MealInfo.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export const MealInfo =(props) => {
+    console.log(props);
+    //object restucturing 
+    //const {idCategory,strCategory,strCategoryDescription,strCategoryThumb}=props.MealInfo
+	return(
+        <>
+       {/* <div className="meal-content">
+        <h3>{strCategory}</h3> <br/>
+        <img src={props.MealInfo.strCategoryThumb} alt="" />
+       </div> */}
+        </>
+    )
+}
